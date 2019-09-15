d2f3ecfe54e3b213ba58548dfe4bd64dc39d48aa
Wojcirej <wojciech.bal@epoczta.pl>
2019-09-15 21:05:43 +0200

    Eliminate double extension in uploaded photo filename.

--- app/domain/photo_file_uploader.rb
+++ app/domain/photo_file_uploader.rb
@@ -2,10 +2,16 @@ class PhotoFileUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   def public_id
-    "photo-gallery_uploads/#{model.uploader_id}/#{model.file.send(:original_filename)}"
+    "photo-gallery_uploads/#{model.uploader_id}/#{model.id}_#{filename_without_extension}"
   end
 
   def extension_whitelist
     %w(png jpg jpeg gif)
+  end
+
+  private
+
+  def filename_without_extension
+    model.file.file.filename.gsub(".#{model.file.file.extension}", "")
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,8 +4,14 @@ class Photo < ApplicationRecord
 
   def self.add_new_for_admin(params, admin_id)
     Photo.create(
+      id: SecureRandom.uuid,
       file: params[:file],
       uploader_id: admin_id,
     )
+  end
+
+  def original_filename
+    return nil unless file.filename
+    file.filename.split("/").last.split("_").last
   end
 end
