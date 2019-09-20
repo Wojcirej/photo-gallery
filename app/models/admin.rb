94a6e17bcb4a8c5aa5b8610c652e9432a0f494b0
Wojcirej <wojciech.bal@epoczta.pl>
2019-09-20 23:36:43 +0200

    Enable edit admin's profile.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,9 +1,6 @@
 class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :validatable,
-         authentication_keys: [:login]
-  #:registerable, :rememberable, :confirmable
+         :registerable, authentication_keys: [:login]
 
   validates :login, presence: true
   has_many :photos, foreign_key: :uploader_id
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "web/base#index"
-  devise_for :admins
+  devise_for :admins, :skip => [:registrations]
+  as :admin do
+    get "admins/edit" => "devise/registrations#edit", :as => "edit_admin_registration"
+    put "admins" => "devise/registrations#update", :as => "admin_registration"
+  end
   namespace :web, path: "/", as: "" do
     resources :photos, only: [:new, :create]
   end
